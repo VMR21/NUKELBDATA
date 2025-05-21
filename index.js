db6e8b116ab7a7490b587ccdd3de136a5b08d2da
VMR21 <reddymodith@gmail.com>
2025-05-22 03:08:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const API_URL = "https://services.rainbet.com/v1/external/affiliates?start_at=2025-05-01&end_at=2025-05-31&key=hzN1uN9CzfYpjbK1SZW84BUl0cml3M5B";
-const SELF_URL = "https://projectgambadata.onrender.com/leaderboard/top14";
+const SELF_URL = "https://nukelbdata.onrender.com/leaderboard/top14";
 
 let cachedData = [];
 
